f026b469b4ce1e7ed7eeae99b545cc296c58b63a
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2023-06-19 11:23:26 +0200

    Add initial cell formulas

--- sheetreader/sheet.go
+++ sheetreader/sheet.go
@@ -6,7 +6,12 @@ import (
 	"github.com/bruyss/go-object-generator/logger"
 )
 
-func AddSheet(f *excelize.File, sheetName string, columns []string) int {
+type colFormula struct {
+	column  int
+	formula string
+}
+
+func addSheet(f *excelize.File, sheetName string, columns []string, colFormulas ...colFormula) int {
 	index, err := f.NewSheet(sheetName)
 	if err != nil {
 		logger.Sugar.Fatal(err)
@@ -31,22 +36,54 @@ func AddSheet(f *excelize.File, sheetName string, columns []string) int {
 	})
 	if err != nil {
 		logger.Sugar.Errorf(err.Error(),
-			"sheet", sheetName
+			"sheet", sheetName,
+		)
+	}
+
+	for _, cf := range colFormulas {
+		cell, _ := excelize.CoordinatesToCellName(cf.column+1, 2)
+		f.SetCellFormula(
+			sheetName,
+			cell,
+			cf.formula,
 		)
 	}
 
 	return index
 }
 
+// InitializeWorkbook creates a new workbook for generation
 func InitializeWorkbook(name string) {
 	f := excelize.NewFile()
 	_ = addSheet(f, sheetMeasmons, measmonCols)
 	_ = addSheet(f, sheetDigmons, digmonCols)
-	_ = addSheet(f, sheetValves, valveCols)
-	_ = addSheet(f, sheetControlValves, controlValveCols)
-	_ = addSheet(f, sheetMotors, motorCols)
-	_ = addSheet(f, sheetFreqMotors, freqMotorCols)
-	_ = addSheet(f, sheetDigouts, digoutCols)
+	_ = addSheet(f, sheetValves, valveCols,
+		colFormula{int(valveOutput), `=$A2`},
+		colFormula{int(valveFeedbackOpenTag), `=$A2 & "_FBO"`},
+		colFormula{int(valveFeedbackClosedTag), `=$A2 & "_FBC"`},
+	)
+	_ = addSheet(f, sheetControlValves, controlValveCols,
+		colFormula{int(controlValveOutput), `=$A2`},
+		colFormula{int(controlValveFeedbackTag), `=$A2 & "_FB"`},
+	)
+	_ = addSheet(f, sheetMotors, motorCols,
+		colFormula{int(motorOutput), `=$A2`},
+		colFormula{int(motorFeedbackTag), `=$A2 & "_FB"`},
+		colFormula{int(motorBreakerTag), `=$A2 & "_TH"`},
+		colFormula{int(motorSwitchTag), `=$A2 & "_WS"`},
+	)
+	_ = addSheet(f, sheetFreqMotors, freqMotorCols,
+		colFormula{int(freqMotorOutput), `=$A2`},
+		colFormula{int(freqMotorFeedbackTag), `=$A2 & "_FB"`},
+		colFormula{int(freqMotorBreakerTag), `=$A2 & "_TH"`},
+		colFormula{int(freqMotorSwitchTag), `=$A2 & "_WS"`},
+		colFormula{int(freqMotorAlarmTag), `=$A2 & "_AL"`},
+	)
+	_ = addSheet(f, sheetDigouts, digoutCols,
+		colFormula{int(digoutOutput), `=$A2`},
+		colFormula{int(digoutFeedbackTag), `=$A2 & "_FB"`},
+		colFormula{int(digoutBreakerTag), `=$A2 & "_TH"`},
+	)
 
 	f.DeleteSheet(f.GetSheetName(0))
 
