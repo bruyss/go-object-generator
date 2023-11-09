bea80070cf60c8ad3c56a83bb7c8e5073cc41ba6
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2023-11-09 14:30:24 +0100

    plc package docstring

--- plc/objects.go
+++ plc/objects.go
@@ -1,8 +1,20 @@
+// Package plc defines types which correspond to commonly used object types
+// in PLC programming.
+//
+// The currently implemented PLC types are:
+//  - Measmon: measuring and monitoring device
+//  - Control valve: position controlled valve
+//  - Digmon: digital monitoring device
+//  - Digout: digital output
+//  - FreqMotor: frequency controlled motor
+//  - Motor: direct on line motor
+//  - Valve: on-off valve
 package plc
 
+// A PlcObject contains information regarding one plc object instance
 type PlcObject interface {
-	// Tag() string
-	// String() string
+	// InputMap returns a map containing all information needed for object instance generation
 	InputMap() map[string]string
+	// PlcTags returns a list of PlcTags associated with the object instance
 	PlcTags() []*PlcTag
 }
